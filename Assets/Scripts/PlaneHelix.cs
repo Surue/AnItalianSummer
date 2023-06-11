ba92912e617dcc243e6a530d958c762eae6c96e6
Surue <nicolas06062000@hotmail.com>
2023-06-11 14:19:44 +0200

    Add script to make helix rotate on airplane

---
+++ Assets/Scripts/PlaneHelix.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneHelix : MonoBehaviour
+{
+    [SerializeField] private float _speedModifier = 10;
+    
+    private Rigidbody _body;
+    
+    private void Start()
+    {
+        _body = GetComponentInParent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        var vel = Mathf.Clamp(_body.velocity.magnitude, 0, 50);
+        
+        transform.Rotate(Vector3.right, vel * Time.deltaTime * _speedModifier);
+    }
+}
